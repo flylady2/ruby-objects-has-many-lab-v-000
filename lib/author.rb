369d81780308647a128e6b029e8468a700e28ee2
Leslie Stevens <flylady@mail.utexas.edu>
2018-06-06 00:22:13 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -10,6 +10,14 @@ class Author
     @posts
   end
 
+  def add_post(post)
+    post.author = self
+  end
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
+  end
+
 
 
 end
